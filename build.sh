880dcf61bfde85b6a8af44f4ac4f9033575ac344
mmsyst <miki.masayuki.mh1@is.naist.jp>
2023-01-26 22:56:49 +0900

    first

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd `dirname $0`
+set -euxo pipefail
+
+REPOSITORY=pycharm-p
+TAG=23.01.26
+IS_GPU=0
+
+TMP_NAME=$(docker image ls --format "{{.ID}}" --filter "reference=${REPOSITORY}:base")
+if [ "${TMP_NAME}" = "" ]; then
+  DOCKER_BUILDKIT=1 docker build --no-cache -t ${REPOSITORY}:base -f ./Dockerfile.base .
+fi
+if [ ${IS_GPU} -eq 1 ]; then
+  DOCKER_BUILDKIT=1 docker build --no-cache -t ${REPOSITORY}:${TAG} -f ./Dockerfile.gpu .
+else
+  DOCKER_BUILDKIT=1 docker build --no-cache -t ${REPOSITORY}:${TAG} -f ./Dockerfile.cpu .
+fi
